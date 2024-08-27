d009ca3034e3b81f192e089d2db14fa2448adb21
Nathnael Chanyalew <134805961+nchanyal@users.noreply.github.com>
2024-08-27 10:37:31 -0400

    feat: add tabbed browsing for the about page

---
+++ src/about.js
@@ -0,0 +1,14 @@
+function generateAboutPage() {
+    const contentDiv = document.querySelector("#content");
+    const header = document.createElement("h1");
+    const paragraph = document.createElement("p");
+
+    contentDiv.textContent = "";
+    header.textContent = "Seriously... there is nothing to see here...";
+    paragraph.textContent = "This website is simply a way for me to test using Webpack."
+
+    contentDiv.appendChild(header);
+    contentDiv.appendChild(paragraph);
+}
+
+export {generateAboutPage};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { generateHomepage } from "./homepage.js";
 import { generateMenuPage } from "./menu-page.js";
+import { generateAboutPage } from "./about.js";
 
 const homeButton = document.querySelector("#home");
 const menuButton = document.querySelector("#menu");
+const contactButton = document.querySelector("#about");
 
 generateHomepage();
 
 homeButton.addEventListener("click", generateHomepage);
 menuButton.addEventListener("click", generateMenuPage);
+contactButton.addEventListener("click", generateAboutPage);
